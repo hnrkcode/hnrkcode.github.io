8c7e11c6654f455f87ca668cab57baf4adfb58de
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-03-12 11:37:01 +0100

    Improve background particles

--- src/_assets/js/index.js
+++ src/_assets/js/index.js
@@ -1,13 +1,81 @@
+import "particles.js";
+
+const particleConfig = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 1000,
+      },
+    },
+    color: {
+      value: "#8d99ae",
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 0,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#8d99ae",
+      opacity: 0.7,
+      width: 1.5,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+};
+
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    particleground(document.getElementById("particles"), {
-      dotColor: "#8d99ae",
-      lineColor: "#8d99ae",
-      particleRadius: 0,
-      proximity: 150,
-    });
-    var intro = document.getElementById("intro");
+    window.particlesJS("particles", particleConfig);
+    const intro = document.getElementById("intro");
     intro.style.marginTop = -intro.offsetHeight / 2 + "px";
   },
   false
